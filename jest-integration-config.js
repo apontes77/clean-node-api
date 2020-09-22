1353d98e665bff5fd934a9e2b29e1d81621b8381
apontes77 <alexandrepontesgyn@gmail.com>
2020-09-22 12:05:13 -0300

    create custom configs to unit and integration tests

---
+++ jest-integration-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.tesMatch = ['**/*.test.js']
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.tesMatch = ['**/*.spec.js']
+module.exports = config
